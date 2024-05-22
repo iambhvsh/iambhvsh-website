478f0e201cf7fb5b24678eb41c8351669671fd37
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-05-22 13:32:59 +0530

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,17 +1,17 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Layout from '../components/Layout/Layout';
+import Navbar from '../components/Layout/Navbar';
 import Container from '../components/Layout/Container';
 
 function Custom404() {
   return (
-    <Layout>
+    <Navbar>
     <Container>
       <h2 className="text-4xl h-screen text-center">
         The page you’re looking for can’t be found.
       </h2>
     </Container>
-  </Layout>
+  </Navbar>
   );
 }
 
