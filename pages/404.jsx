d8ca531efd412c145f16d2c9965d810702a7f61e
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-05-22 13:21:28 +0530

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,26 +1,16 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../components/Layout/Layout';
+import Container from '../components/Layout/Container';
 
 function Custom404() {
   return (
     <Layout>
-    <div className="flex flex-col items-center justify-center h-screen w-screen bg-black text-white">
-      <Head>
-        <title>Page Not Found</title>
-      </Head>
-      <div className="text-center">
-        <div className="text-center">
-          <h2 className="text-4xl">404</h2> {/* Decrease font size for better readability */}
-          <p className="text-md mb-8 ml-sm mr-sm">The page you're looking for doesn't seem to exist.</p> {/* Decrease font size for better readability */}
-        </div>
-        <div className="mt-8">
-          <Link href="/" className="px-6 py-3 bg-blue-500 text-white rounded-full hover:bg-white transition-colors duration-300">
-            Go back to the home
-          </Link>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <h2 className="text-6xl">
+        The page you’re looking for can’t be found.
+      </h2>
+    </Container>
   </Layout>
   );
 }
