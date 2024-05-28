1907525fed5002d41eeadadd2de9743cfd42a1ed
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-05-28 12:03:53 +0530

    Update Intro.jsx

--- components/Home/Intro.jsx
+++ components/Home/Intro.jsx
@@ -1,8 +1,10 @@
+import Highlight from '../Utils/Highlight';
+
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl lg:text-9xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blogs.
+        <Highlight>iambhvsh</Highlight>
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         Hi&nbsp;
