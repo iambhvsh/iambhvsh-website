368e1cec6cb6030a527c26ce7b6280ac66c3333f
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-05-30 12:14:33 +0530

    Update Highlight.jsx

--- components/Utils/Highlight.jsx
+++ components/Utils/Highlight.jsx
@@ -1,6 +1,6 @@
 export default function Highlight({ children }) {
   return (
-    <span className="hover:text-transparent bg-clip-text bg-gradient-to-r bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
+    <span className="text-transparent bg-clip-text bg-gradient-to-r bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
       {children}
     </span>
   )
