6540484149608ad0ab42b4935a0b6e0d2ff5d52d
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-07-01 08:53:31 +0530

    Update Nav.jsx

--- components/Layout/Nav.jsx
+++ components/Layout/Nav.jsx
@@ -20,40 +20,38 @@ export default function Nav({ darkMode, setDarkMode }) {
   const currentPath = router.pathname;
 
   return (
-    <Disclosure as="nav" className="bg-white dark:bg-gray-900 shadow fixed top-0 w-full z-30">
+    <Disclosure as="nav" className="fixed top-0 w-full z-30 backdrop-blur-md bg-opacity-50">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
-              <div className="flex">
-                <div className="flex-shrink-0 flex items-center">
-                  <Highlight>
-                    <Link href="/">
-                      <a className="font-bold italic text-2xl text-gray-900 dark:text-white">iambhvsh</a>
-                    </Link>
-                  </Highlight>
-                </div>
-                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                  {navigation.map((item) => (
-                    <Link key={item.name} href={item.href}>
-                      <a
-                        className={classNames(
-                          currentPath === item.href
-                            ? 'border-indigo-500 text-gray-900 dark:text-white'
-                            : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:hover:text-gray-300',
-                          'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
-                        )}
-                      >
-                        {item.name}
-                      </a>
-                    </Link>
-                  ))}
-                </div>
+              <div className="flex items-center">
+                <Highlight>
+                  <Link href="/">
+                    <a className="font-bold italic text-2xl text-gray-900 dark:text-white">iambhvsh</a>
+                  </Link>
+                </Highlight>
               </div>
-              <div className="hidden sm:ml-6 sm:flex sm:items-center">
+              <div className="hidden sm:flex sm:items-center sm:space-x-8">
+                {navigation.map((item) => (
+                  <Link key={item.name} href={item.href}>
+                    <a
+                      className={classNames(
+                        currentPath === item.href
+                          ? 'border-indigo-500 text-gray-900 dark:text-white'
+                          : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:hover:text-gray-300',
+                        'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                      )}
+                    >
+                      {item.name}
+                    </a>
+                  </Link>
+                ))}
+              </div>
+              <div className="hidden sm:flex sm:items-center">
                 <Toggle darkMode={darkMode} setDarkMode={setDarkMode} />
               </div>
-              <div className="-mr-2 flex items-center sm:hidden">
+              <div className="flex items-center sm:hidden">
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -66,7 +64,7 @@ export default function Nav({ darkMode, setDarkMode }) {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
+          <Disclosure.Panel className="sm:hidden backdrop-blur-md bg-opacity-50">
             <div className="pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
                 <Link key={item.name} href={item.href}>
